4b4c8a506396f697948ef1ee8fc0b9f3b1d29a62
Ayush Agarwal <dearayush2@gmail.com>
2024-08-08 12:32:54 +0530

    Backend

--- client/src/components/Username.js
+++ client/src/components/Username.js
@@ -1,23 +1,29 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link,useNavigate } from 'react-router-dom'
 import avatar from '../assets/profile.png';
 import { Toaster } from 'react-hot-toast';
 import { useFormik } from 'formik';
 import { usernameValidate } from '../helper/validate'
+import { useAuthStore } from '../store/store'
 
 import styles from '../styles/Username.module.css';
 
 export default function Username() {
 
+  const navigate = useNavigate();
+  const setUsername = useAuthStore(state => state.setUsername);
+
   const formik = useFormik({
     initialValues : {
-      username : ''
+      username : 'example123'
     },
     validate : usernameValidate,
     validateOnBlur: false,
     validateOnChange: false,
     onSubmit : async values => {
+      setUsername(values.username);
       console.log(values)
+      //navigate('/password')
     }
   })
 
